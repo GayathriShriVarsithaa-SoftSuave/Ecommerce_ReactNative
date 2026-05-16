0c1fb156d5cd2fc53038f7bdd956cb6da81758d9
GayathriShriVarsithaa <gayathrishri.varsithaa@softsuave.org>
2026-05-16 15:00:35 +0530

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
@@ -13,11 +7,9 @@ import {
 } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
     </SafeAreaProvider>
   );
